7b88f4791032e833dd0e650b34c41ed8b367bc9d
Viacheslav Matrizaev <matrizaev@gmail.com>
2022-01-17 11:37:27 +0500

    Sensitive information removed

--- config.py
+++ config.py
@@ -5,18 +5,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 
-class Config(object):
+class Config:
     APPLICATION_TITLE = (
         os.environ.get('APPLICATION_TITLE') or 'Application Title'
     )
-    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL') or 'admin@pushkind.com'
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'PushkindDotCom'
+    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL') or 'admin@example.com'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     ICU_EXTENSION_PATH = os.path.join(basedir, 'libsqliteicu.so')
     SQLALCHEMY_DATABASE_URI = (
         os.environ
         .get('DATABASE_URL', 'file:' + os.path.join(basedir, 'app.db'))
         .replace('file:', 'sqlite:///', 1)
     )
+    ECWID_JS_URL = os.environ.get('ECWID_JS_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
