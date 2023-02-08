cea0cdf9e92e5738903677f2c8fbce4da1c8bcad
Viacheslav Matrizaev <Viacheslav.Matrizaev2@nike.com>
2023-02-09 00:02:19 +0300

    isorted

--- approve.py
+++ approve.py
@@ -1,36 +1,53 @@
-from app import create_app, db, cli
-from app.models import User, UserRoles, Vendor, OrderApproval, OrderStatus
-from app.models import UserCategory, UserProject, OrderPosition, OrderCategory
-from app.models import EventType, IncomeStatement, CashflowStatement
-from app.models import Category, AppSettings, OrderEvent, Project, Site, Order
-from app.models import Position, OrderLimit
-
+from app import cli, create_app, db
+from app.models import (
+    AppSettings,
+    CashflowStatement,
+    Category,
+    EventType,
+    IncomeStatement,
+    Order,
+    OrderApproval,
+    OrderCategory,
+    OrderEvent,
+    OrderLimit,
+    OrderPosition,
+    OrderStatus,
+    Position,
+    Project,
+    Site,
+    User,
+    UserCategory,
+    UserProject,
+    UserRoles,
+    Vendor,
+)
 
 application = create_app()
 cli.register(application)
 
+
 @application.shell_context_processor
 def make_shell_context():
     return {
-        'db': db,
-        'User': User,
-        'UserRoles': UserRoles,
-        'Vendor': Vendor,
-        'OrderApproval': OrderApproval,
-        'OrderStatus': OrderStatus,
-        'Category': Category,
-        'AppSettings': AppSettings,
-        'OrderEvent': OrderEvent,
-        'Project': Project,
-        'Site': Site,
-        'Position': Position,
-        'Order': Order,
-        'UserCategory': UserCategory,
-        'UserProject': UserProject,
-        'OrderPosition': OrderPosition,
-        'OrderCategory': OrderCategory,
-        'EventType': EventType,
-        'IncomeStatement': IncomeStatement,
-        'CashflowStatement': CashflowStatement,
-        'OrderLimit': OrderLimit
+        "db": db,
+        "User": User,
+        "UserRoles": UserRoles,
+        "Vendor": Vendor,
+        "OrderApproval": OrderApproval,
+        "OrderStatus": OrderStatus,
+        "Category": Category,
+        "AppSettings": AppSettings,
+        "OrderEvent": OrderEvent,
+        "Project": Project,
+        "Site": Site,
+        "Position": Position,
+        "Order": Order,
+        "UserCategory": UserCategory,
+        "UserProject": UserProject,
+        "OrderPosition": OrderPosition,
+        "OrderCategory": OrderCategory,
+        "EventType": EventType,
+        "IncomeStatement": IncomeStatement,
+        "CashflowStatement": CashflowStatement,
+        "OrderLimit": OrderLimit,
     }
