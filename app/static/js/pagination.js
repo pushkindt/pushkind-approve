6342e0e77f2a4070779d64837f91c60d6d47b980
Viacheslav Matrizaev <Viacheslav.Matrizaev2@nike.com>
2023-03-14 14:05:01 +0300

    Added pagination.js

---
+++ app/static/js/pagination.js
@@ -0,0 +1,49 @@
+
+
+function InstantiatePagination(itemsWrapperId, itemClass, paginationId, itemsPerPage) {
+    const pagination = document.getElementById(paginationId);
+    const itemsWrapper = document.getElementById(itemsWrapperId);
+    const items = itemsWrapper.querySelectorAll(`.${itemClass}`);
+    const pageCount = Math.ceil(items.length / itemsPerPage);
+    pagination.innerHTML = "";
+
+    const setCurrentPage = (pageNum) => {
+        const prevRange = (pageNum - 1) * itemsPerPage;
+        const currRange = pageNum * itemsPerPage;
+        const pageLinks = pagination.querySelectorAll("li.page-item");
+        pageLinks.forEach((link) => { link.classList.remove("active") });
+        const currentLink = pagination.querySelector(`li.page-item[data-page="${pageNum}"]`);
+        currentLink.classList.add("active");
+        items.forEach((item, index) => {
+            const showItem = (index >= prevRange && index < currRange);
+            item.style.display = showItem ? "flex" : "none";
+        });
+    }
+
+    const appendPageNumber = (index, content) => {
+        const li = document.createElement("li");
+        li.className = "page-item";
+        li.dataset.page = index;
+        const a = document.createElement("a");
+        a.textContent = index;
+        a.className = "page-link";
+        a.href = "#";
+        li.addEventListener("click", () => {
+            setCurrentPage(index);
+        });
+        li.appendChild(a);
+        content.appendChild(li);
+    };
+
+    const getPaginationNumbers = (pageCount) => {
+        const content = document.createDocumentFragment();
+        for (let i = 1; i <= pageCount; i++) {
+            appendPageNumber(i, content);
+        }
+        return content;
+    };
+
+    pagination.appendChild(getPaginationNumbers(pageCount));
+    setCurrentPage(1);
+}
+
